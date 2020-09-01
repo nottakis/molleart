ed356f21034022616bba301976ade578915e4cac
Taki <tk@noubies.com>
2020-09-01 04:49:36 +0100

    :bug: Unify the path resolution of static assets

--- src/pages/index.js
+++ src/pages/index.js
@@ -60,7 +60,8 @@ function Home() {
       title={`Hello from ${siteConfig.title}`}
       description="Description will go into a meta tag in <head />">
       <header className={clsx('hero hero--primary', styles.heroBanner)}>
-        <div className="container">
+        <img className="hero__image" src={useBaseUrl("img/hero.png")}/>
+        <div className="container" style={{ position: "relative"}}>
           <h1 className="hero__title">{siteConfig.title}</h1>
           <p className="hero__subtitle">{siteConfig.tagline}</p>
           <div className={styles.buttons}>
