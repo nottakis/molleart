174d8aa4a256b53dcee88b4f0545e41000209b29
Taki <tk@noubies.com>
2020-09-14 01:23:17 +0100

    :art: Move css to component

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -10,10 +10,10 @@ function Header() {
     const context = useDocusaurusContext();
     const {siteConfig = {}} = context;
     return (
-      <header className={clsx('hero hero--primary', styles.heroBanner)}>
-        <img className="hero__image" src={useBaseUrl("img/hero.png")}/>
+      <header className={clsx('hero hero--primary', styles.hero)}>
+        <img className={clsx('hero__image', styles.heroImage)} src={useBaseUrl("img/hero.png")}/>
         <div className="container" style={{ position: "relative"}}>
-          <h1 className="hero__title">{siteConfig.title}</h1>
+          <h1 className={clsx('hero__title', styles.heroTitle)}>{siteConfig.title}</h1>
         </div>
         <div style={{ position: "absolute", top: 0, right: 0, padding: 10 }}>
           <button className="button button--secondary" data-dh-feature="network" data-dh-property-enable="true">
