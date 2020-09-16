8a8f4fa531cc20f06eef4795dbecd0a776ed59ab
Taki <tk@noubies.com>
2020-09-16 23:22:47 +0100

    :construction: Format chart

--- src/components/Chart/Chart.js
+++ src/components/Chart/Chart.js
@@ -2,39 +2,108 @@
 import React from 'react';
 import styles from './styles.module.scss';
 
-import { Bar } from '@nivo/bar'
+import { ResponsiveBar } from '@nivo/bar'
+import data from './data'
 
-const keys = ['Alice', 'Bob', 'Cathy', 'Darren', 'Emily', 'Frank']
-const commonProps = {
-  width: 900,
-  height: 500,
-  margin: { top: 60, right: 80, bottom: 60, left: 80 },
-  data: [{ 'Alice': 80, 'Bob': 80, 'Cathy': 80, 'Darren': 80, 'Emily': 80, 'Frank': 80 }],
-  indexBy: 'country',
-  keys,
-  padding: 0.2,
-  labelTextColor: 'inherit:darker(1.4)',
-  labelSkipWidth: 16,
-  labelSkipHeight: 16,
-}
-
+/*
+  Cf. https://nivo.rocks/bar/
+*/
 function Chart() {
   return (
     <div className="container">
-      <Bar
-        {...commonProps}
-        axisLeft={{
-            format: value =>
-                `$${Number(value).toLocaleString('en-US', {
-                    minimumFractionDigits: 0,
-                })}`,
-        }}
-        tooltipFormat={value =>
-            `$${Number(value).toLocaleString('en-US', {
-                minimumFractionDigits: 0,
-            })}`
-        }
+      <div className={styles.chart}>
+        <ResponsiveBar
+          data={data}
+          keys={['Alice', 'Bob', 'Cathy', 'Darren', 'Emily', 'Frank']}
+          indexBy="order"
+          margin={{ top: 50, right: 130, bottom: 50, left: 60 }}
+          padding={0.3}
+          colors={{ scheme: 'greys' }}
+          defs={[
+              {
+                  id: 'dots',
+                  type: 'patternDots',
+                  background: 'inherit',
+                  color: '#38bcb2',
+                  size: 4,
+                  padding: 1,
+                  stagger: true
+              },
+              {
+                  id: 'lines',
+                  type: 'patternLines',
+                  background: 'inherit',
+                  color: '#eed312',
+                  rotation: -45,
+                  lineWidth: 6,
+                  spacing: 10
+              }
+          ]}
+          fill={[
+              {
+                  match: {
+                      id: 'fries'
+                  },
+                  id: 'dots'
+              },
+              {
+                  match: {
+                      id: 'sandwich'
+                  },
+                  id: 'lines'
+              }
+          ]}
+          borderColor={{ from: 'color', modifiers: [ [ 'darker', 1.6 ] ] }}
+          axisTop={null}
+          axisRight={null}
+          axisBottom={{
+              tickSize: 5,
+              tickPadding: 5,
+              tickRotation: 0,
+              legend: 'Order of Transactions',
+              legendPosition: 'middle',
+              legendOffset: 32
+          }}
+          axisLeft={{
+              tickSize: 5,
+              tickPadding: 5,
+              tickRotation: 0,
+              legend: 'Total Earnings',
+              legendPosition: 'middle',
+              legendOffset: -40
+          }}
+          labelSkipWidth={12}
+          labelSkipHeight={12}
+          labelTextColor={{ from: 'color', modifiers: [ [ 'darker', 1.6 ] ] }}
+          legends={[
+              {
+                  dataFrom: 'keys',
+                  anchor: 'bottom-right',
+                  direction: 'column',
+                  justify: false,
+                  translateX: 120,
+                  translateY: 0,
+                  itemsSpacing: 2,
+                  itemWidth: 100,
+                  itemHeight: 20,
+                  itemDirection: 'left-to-right',
+                  itemOpacity: 0.85,
+                  symbolSize: 20,
+                  effects: [
+                      {
+                          on: 'hover',
+                          style: {
+                              itemOpacity: 1
+                          }
+                      }
+                  ]
+              }
+          ]}
+          animate={true}
+          motionStiffness={90}
+          motionDamping={15}
       />
+      </div>
     </div>
   );
 }
---
+++ src/components/Chart/data.js
@@ -0,0 +1,59 @@
+
+const data = [
+    {
+      "order": "1st",
+      "Alice": 193,
+      "Bob": 66,
+      "Cathy": 90,
+      "Darren": 10,
+      "Emily": 125,
+      "Frank": 166,
+    },
+    {
+      "order": "2nd",
+      "Alice": 22,
+      "Bob": 84, 
+      "Cathy": 7,
+      "Darren": 18,
+      "Emily": 146,
+      "Frank": 113,
+    },
+    {
+      "order": "3rd",
+      "Alice": 124,
+      "Bob": 114,
+      "Cathy": 18,
+      "Darren": 166,
+      "Emily": 12,
+      "Frank": 111,
+    },
+    {
+      "order": "4th",
+      "Alice": 92,
+      "Bob": 161,
+      "Cathy": 16,
+      "Darren": 196,
+      "Emily": 124,
+      "Frank": 42,
+    },
+    {
+      "order": "5th",
+      "Alice": 155,
+      "Bob": 125,
+      "Cathy": 56,
+      "Darren": 162,
+      "Emily": 163,
+      "Frank": 179,
+    },
+    {
+      "order": "6th",
+      "Alice": 108,
+      "Bob": 113,
+      "Cathy": 170,
+      "Darren": 1,
+      "Emily": 34,
+      "Frank": 162,
+    },
+]
+
+export default data
