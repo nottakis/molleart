6086f94066e6183b8adf6dc8efb3860fe25b8ea5
Taki <tk@noubies.com>
2020-09-17 02:11:43 +0100

    :wrench: Add config url

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -88,7 +88,7 @@ module.exports = {
           sidebarPath: require.resolve('./sidebars.js'),
           // Please change this to your repo.
           editUrl:
-            'https://github.com/facebook/docusaurus/edit/master/website/',
+            'https://github.com/Mol-LeArt/mvp/edit/master/',
         },
         theme: {
           customCss: require.resolve('./src/css/custom.scss'),
